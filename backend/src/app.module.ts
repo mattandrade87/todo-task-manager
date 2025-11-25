bdf79739a3087a977ea2b93b0e16de68d4e8b017
mattandrade87 <mateus.vitor.andrade@gmail.com>
2025-11-25 11:58:39 -0300

    feat: adiciona configModule para ler arquivos .env

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { TasksModule } from './tasks/tasks.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, TasksModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    PrismaModule,
+    TasksModule,
+    ConfigModule.forRoot({
+      isGlobal: true, // ConfigModule global
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
